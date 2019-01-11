a2cbb343169d0943624ae003719e33ec07bd2c57
Jakson Kallio <jakson@protonmail.com>
2019-01-11 13:06:36 -0600

    GeoCoord equals and hashCode

--- GeoGraphPathing/src/geographpathing/GeoCoord.java
+++ GeoGraphPathing/src/geographpathing/GeoCoord.java
@@ -10,6 +10,25 @@ public class GeoCoord {
 		return 0;
 	}
 	
+	@Override
+	public int hashCode(){
+		int tmp = y + ((x + 1) / 2);
+        return (x + (int) Math.pow(tmp, 2));
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof GeoCoord){
+			GeoCoord other = (GeoCoord) o;
+			
+			if(other.x == this.x && other.y == this.y){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public final int x;
 	public final int y;
 }
